273b63e1c732c80ed6e60eb60c0e3bf3071d4728
Danielthweatt <dthweatt192@yahoo.com>
2018-05-20 14:54:12 -0500

    worked on model

--- models/burger.js
+++ models/burger.js
@@ -3,9 +3,21 @@ const orm = require("../config/orm.js");
 
 // Model
 const burger = {
-    selectAll: function(){},
-    insertOne: function(){},
-    updateOne: function(){}
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(result){
+            cb(result);
+        });
+    },
+    insertOne: function(columns, values, cb){
+        orm.insertOne("burgers", columns, values, function(result){
+            cb(result);
+        });
+    },
+    updateOne: function(newColumnValues, condition, cb){
+        orm.updateOne("burgers", newColumnValues, condition, function(result){
+            cb(result);
+        });
+    }
 };
 
 // Export
