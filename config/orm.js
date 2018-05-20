7d1318951eef93ae2c8a7323924c564c2ae513ba
Danielthweatt <dthweatt192@yahoo.com>
2018-05-19 23:55:20 -0500

    worked on ORM

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,34 @@
 // Modules
 const connection = require("../config/connection.js");
 
+// Helpers
+function questionMarkify(values){
+    return values.map(function(element){
+        return "?";
+    }).toString();
+};
+
 // ORM
 const orm = {
-    selectAll: function(){},
-    insertOne: function(){},
+    selectAll: function(table, cd){
+        connection.query(`SELECT * FROM ${table}`, function(error, result){
+            if (error) {
+                console.log(`Oh boy, it broke: ${error}`);
+            } else {
+                cb(result);
+            }
+        });
+    },
+    insertOne: function(table, columns, values, cb){
+        connection.query(`INSERT INTO ${table} (${columns.toString()}) VALUES (${questionMarkify(values)})`, 
+        function(error, result){
+            if (error) {
+                console.log(`Oh boy, it broke: ${error}`);
+            } else {
+                cb(result);
+            }
+        });
+    },
     updateOne: function(){}
 };
 
